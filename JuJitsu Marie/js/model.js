8d8bfaafec38596d2a910a7148189efac09281a0
Ole H <olehagen@gmail.com>
2023-10-18 12:07:14 +0200

    Update model exercises del A

--- JuJitsu Marie/js/model.js
+++ JuJitsu Marie/js/model.js
@@ -66,33 +66,282 @@ const model = {
   ],
   exercises: [
 // Katagori 1 har 8 subtyper med totalt 27 øvelser: id 1 til 27
-    // 3 øvelser under Guard/benstilling
-    { id: 1, subtypeId: 1, name: "Forsvarsguard, åpne hender", nameJapanese: "Shuto", description: "" },
-    // Neste to bare for nummerering
-    { id: 2, subtypeId: 1, name: "Angrepsguard, lukkede hender" , nameJapanese: "Seiken" , description: "" },
-    { id: 3, subtypeId: 1, name: "", nameJapanese: "", description: "" },
+    // 1 / 1 Guardstillinger / Benstillinger (Kamae / Ashi-Tachi)
+    {
+      id: 1,
+      subtypes: 1,
+      CategoryId: 1,
+      name: 'Forsvarsguard, åpne hender',
+      nameJapanese: 'Shuto',
+      withPartner: '',
+      description: ''
+    },
+    {
+      id: 2,
+      subtypes: 1,
+      CategoryId: 1,
+      name: 'Angrepsguard, lukkede hender',
+      nameJapanese: 'Seiken',
+      withPartner: '',
+      description: ''
+    },
+    {
+      id: 3,
+      subtypes: 1,
+      CategoryId: 1,
+      name: 'Grunnleggende stilling',
+      nameJapanese: 'Zenkutsu-dachi',
+      withPartner: '',
+      description: ''
+    },
+    // 2 / 1 Forflytninger (Tai-sabaki)
+    {
+      id: 4,
+      subtypes: 2,
+      CategoryId: 1,
+      name: 'Sideforflytning',
+      nameJapanese: 'Yoko-ashi',
+      withPartner: '',
+      description: ''
+    },
+    {
+      id: 5,
+      subtypes: 2,
+      CategoryId: 1,
+      name: 'Kroppsvending',
+      nameJapanese: 'Sha-tenkan',
+      withPartner: '',
+      description: ''
+    },
+    {
+      id: 6,
+      subtypes: 2,
+      CategoryId: 1,
+      name: '90 grader dreining',
+      nameJapanese: 'Han-tenkan',
+      withPartner: '',
+      description: ''
+    },
+    {
+      id: 7,
+      subtypes: 2,
+      CategoryId: 1,
+      name: '180 grader dreining',
+      nameJapanese: 'Zen-tenkan',
+      withPartner: '',
+      description: ''
+    },
+    // 3 / 1 Fallteknikk (Ukemi-waza)
+    {
+      id: 8,
+      subtypes:3,
+      CategoryId: 1,
+      name: 'Sideveis fall',
+      nameJapanese: 'Yoko ukemi',
+      withPartner: false,
+      description: ''
+    },
+    {
+      id: 9,
+      subtypes: 3,
+      CategoryId: 1,
+      name: 'Forover fall',
+      nameJapanese: 'Mae ukemi',
+      withPartner: false,
+      description: ''
+    },
+    {
+      id: 10,
+      subtypes: 3,
+      CategoryId: 1,
+      name: 'Bakover fall',
+      nameJapanese: 'Ushiro ukemi',
+      withPartner: false,
+      description: ''
+    },
+    {
+      id: 11,
+      subtypes: 3,
+      CategoryId: 1,
+      name: 'Forover volt',
+      nameJapanese: 'Kaiten mae ukemi',
+      withPartner: false,
+      description: ''
+    },
+    {
+      id: 12,
+      subtypes: 3,
+      CategoryId: 1,
+      name: 'Bakover volt',
+      nameJapanese: 'Kaiten ushiro ukemi',
+      withPartner: false,
+      description: ''
+    },
+    // 4 / 1 Kast / Fellinger (Nage waza)
+    {
+      id: 13,
+      subtypes: 4,
+      CategoryId: 1,
+      name: 'Stor ytre benfelling',
+      nameJapanese: 'O soto o toshi',
+      withPartner: true,
+      description: ''
+    },
+    {
+      id: 14,
+      subtypes: 4,
+      CategoryId: 1,
+      name: 'Stort hoftekast',
+      nameJapanese: 'O goshi',
+      withPartner: true,
+      description: ''
+    },
+    // 5 / 1 Slag (Tsuki waza)
+    {
+      id: 15,
+      subtypes: 5,
+      CategoryId: 1,
+      name: 'Rett slag med fremste hånd',
+      nameJapanese: 'Tobikomi zuki',
+      withPartner: '',
+      description: ''
+    },
+    {
+      id: 16,
+      subtypes: 5,
+      CategoryId: 1,
+      name: 'Rett slag med bakerste hånd',
+      nameJapanese: 'Gyaku zuki',
+      withPartner: '',
+      description: ''
+    },
+    {
+      id: 17,
+      subtypes: 5,
+      CategoryId: 1,
+      name: 'Slagkombinasjon med rette slag',
+      nameJapanese: 'Tobikomi zuki / Gyaku zuki',
+      withPartner: '',
+      description: ''
+    },
+    // 6 / 1 Spark (Keri waza)
+    {
+      id: 18,
+      subtypes: 6,
+      CategoryId: 1,
+      name: 'Grunnleggende rett spark med bakerste ben',
+      nameJapanese: 'Gyaku mae geri',
+      withPartner: '',
+      description: ''
+    },
+    {
+      id: 19,
+      subtypes: 6,
+      CategoryId: 1,
+      name: 'Grunnleggende rett spark med fremste ben',
+      nameJapanese: 'Tsurikomi mae geri',
+      withPartner: '',
+      description: ''
+    },
+    // 7 / 1 Mønster (Kata)
+    {
+      id: 20,
+      subtypes: 7,
+      CategoryId: 1,
+      name: 'Go kyu kata - Guard, fem retninger',
+      nameJapanese: 'Zenkutsu-dachi / Shuto',
+      withPartner: '',
+      description: ''
+    },
+    // 8 / 1 Blokkeringer (Uke waza)
+    {
+      id: 21,
+      subtypeId: 8, 
+      CategoryId: 1,
+      name: 'Lav blokk, innenfra og ut',
+      nameJapanese: 'Gedan barai',
+      withPartner: '',
+      description: ''
+    },
+    {
+      id: 22,
+      subtypeId: 8, 
+      CategoryId: 1,
+      name: 'Mellom blokk, innenfra og ut',
+      nameJapanese: 'Uchi-uke',
+      withPartner: '',
+      description: ''
+    },
+    {
+      id: 23,
+      subtypeId: 8, 
+      CategoryId: 1,
+      name: 'Høy blokk, nedenfra og opp',
+      nameJapanese: 'Age-uke',
+      withPartner: '',
+      description: ''
+    },
+    {
+      id: 24,
+      subtypeId: 8, 
+      CategoryId: 1,
+      name: 'Mellom blokk, utenfra og inn',
+      nameJapanese: 'Soto-uke',
+      withPartner: '',
+      description: ''
+    },
+    {
+      id: 25,
+      subtypeId: 8, 
+      CategoryId: 1,
+      name: 'Lav blokk, unenfra og inn',
+      nameJapanese: 'Sukui-uke',
+      withPartner: '',
+      description: ''
+    },
+    {
+      id: 26,
+      subtypeId: 8, 
+      CategoryId: 1,
+      name: 'Lav kryssblokk, ovenfra og ned',
+      nameJapanese: 'Gedan juji-uke',
+      withPartner: '',
+      description: ''
+    },
+    {
+      id: 27,
+      subtypeId: 8,
+      CategoryId: 1,
+      name: 'Høy kryssblokk, nedenfra and opp',
+      nameJapanese: 'Jodan juji-uke',
+      withPartner: '',
+      description: ''
+    },
 
-    // 4 Øvelser under Forflytninger 
-    { id: 4, subtypeId: 1, name: "Sideforflytning", nameJapanese: "Yoko-ashi", description: "" },
-    { id: 5, subtypeId: 1, name: "Kroppsvending", nameJapanese: "Sha-tenkan", description: "" },
-    { id: 6, subtypeId: 1, name: "", nameJapanese: "", description: "" },
-    { id: 7, subtypeId: 1, name: "", nameJapanese: "", description: "" },
 
-    // osv for alle 8 subtypene under kategori 1, med totalt 27 øvelser . 
-
+// 
 // Kategori 2 har 4 subtyper med totalt 22 øvelser: id 28 til 49
     // 4 subtyper 
+    {
+      id: 28,
+      subtypeId: 9,
+      CategoryId: 2,
+      name: '',
+      nameTJK: '',
+      withPartner: '',
+      description: ''
+    },
 
-    { id: 28, subtypeId: 9, name: "Forsvar mot hårtak", nameJapanese: null, description: "Høyre hånd holder motstanders høyre hånd +++ " }, 
+    { id: 28, subtypeId: 9, name: "Forsvar mot hårtak", nameJapanese: null, WithPartner: true, description: "Høyre hånd holder motstanders høyre hånd +++ " }, 
     // + 11 øvelser til 
-    { id: 40, subtypeId: 12, name: "Forsvar mot strupetak", nameJapanese: null, description: "Kun forflytninger med god avstand. Knyttneve ved spark-blokk" }, 
+    { id: 40, subtypeId: 12, name: "Forsvar mot strupetak", nameJapanese: null, WithPartner: true, description: "Kun forflytninger med god avstand. Knyttneve ved spark-blokk" }, 
    
 
 
 
-    { id: 2, subtypeId: 1, name: 'Guard2', description: 'Guard2' }, 
+
 // Katagori 3 har 1 subtyper med totalt 1 øvelse med id 50
-    { id: 50, subtypeId: 13, name: "Sirkeltrening" , description: ": Forsvar mot uavtalte kontaktgrep fra gult pensum." }, 
+    { id: 50, subtypeId: 13, name: "Sirkeltrening", WithPartner: true, description: ": Forsvar mot uavtalte kontaktgrep fra gult pensum." }, 
   ],
   media: [
     { id: 1, exerciseId: 1, caption: '511a', type:'img', detail:'guard2.png'},
