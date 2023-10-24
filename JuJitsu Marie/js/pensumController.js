9290464aebcbfbc41ab2638cd8f6ab373da5c080
martinsletsjoe <106916526+martinsletsjoe@users.noreply.github.com>
2023-10-24 13:18:42 +0200

    Update pensumController.js

--- JuJitsu Marie/js/pensumController.js
+++ JuJitsu Marie/js/pensumController.js
@@ -1,18 +1,3 @@
-
-
-function clickedTrainingInfo(exerciseName) {
-    let exerciseContentHtml = '';
-    const infoHtml = model.exercises[exerciseName];
-    exerciseContentHtml = [
-        `<h2>${infoHtml.name || ''}</h2>`,
-        `<h4>${infoHtml.nameJapanese || ''}</h4>`,
-        `<p>${infoHtml.description || ''}</p>`,
-        `<p><iframe src="${infoHtml.medialink}" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
-        </p>`,
-    ];
-    document.getElementById('exerciseContent').innerHTML = exerciseContentHtml.join('');
-}
-
 function selectSubtype(id) {
     model.selectedSubtype = id;
     pensumView();
