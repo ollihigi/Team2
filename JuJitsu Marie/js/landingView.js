9c1c08c74cbd5f165a122b1d6bc5f4674da302a8
Your Name <stiant@getacademy.no>
2023-11-01 08:41:18 +0100

    update 5 files

--- JuJitsu Marie/js/landingView.js
+++ JuJitsu Marie/js/landingView.js
@@ -5,16 +5,17 @@ function landingView() {
     <div class="row">
       <div class="col-md-6 ">
         <div class="detail-box">
-          <h1 id="pensumTitle">KYU Pensum Oversikt</h1>
+          <h1 id="pensumTitle">Ju Jitsu Pensum Oversikt</h1>
           <p id="pensumText">
-           Lag en bruker og se hvor mye du allerede<br>mestrer eller har behov for å forbedre
+          Velkommen til nettstedet vårt for Ju Jitsu-fremdriftssporing.<br><br>
+          Her kan du se fremgangen din mot neste beltegradering prøve på en enkel og effektiv måte.
           </p>
-          <a onclick="authView()" id="pensumMyPage">Lag Bruker</a>
+          <a onclick="authView()" id="pensumMyPage">Lag bruker</a>
         </div>
       </div>
       <div class="col-md-6">
         <div class="img-box">
-          <img src="/JuJitsu Marie/images/landingbg.png" alt="">
+          <img src="./images/landingbg.png" alt="">
     </div>
   </div>
   </div>
